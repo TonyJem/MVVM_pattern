a45459765b730684b99012d315d02db479a4955f
TonyJem <a.jemanov@gmail.com>
2021-07-25 10:06:14 +0300

    Create MvvmModel

--- MVVM_pattern/MVVM_pattern/Model/MvvmModel.swift
+++ MVVM_pattern/MVVM_pattern/Model/MvvmModel.swift
@@ -1,1 +1,13 @@
+import Foundation
 
+enum MvvmModel {
+    case initial
+    case loading(Model)
+    case success(Model)
+    case failure(Model)
+    
+    struct Model {
+        let image: String
+        let isHiden: Bool
+    }
+}
