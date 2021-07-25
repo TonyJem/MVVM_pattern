a768916e78def9b3ede05e10bf7cb43c46952e74
TonyJem <a.jemanov@gmail.com>
2021-07-25 09:01:03 +0300

    Create Outlets and Actions for View

--- MVVM_pattern/MVVM_pattern/View/MvvmView.swift
+++ MVVM_pattern/MVVM_pattern/View/MvvmView.swift
@@ -1,11 +1,21 @@
 import UIKit
 
 class MvvmView: UIViewController {
-
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var showFirstImageBtn: UIButton!
+    @IBOutlet private weak var showSecondImageBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showFirstImageBtnPressed(_ sender: UIButton) {
+        print("🟢 showFirstImageBtnPressed")
+    }
+    
+    @IBAction func showSecondImageBtnPressed(_ sender: UIButton) {
+        print("🟢 showSecondImageBtnPressed")
+    }
 }
