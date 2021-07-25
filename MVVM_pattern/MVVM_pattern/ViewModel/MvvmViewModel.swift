fc21b8b76368471308e7c0c52d1f63ea5f74fd3f
TonyJem <a.jemanov@gmail.com>
2021-07-25 09:06:43 +0300

    Remove ViewModel's code to ViewModel's file

--- MVVM_pattern/MVVM_pattern/ViewModel/MvvmViewModel.swift
+++ MVVM_pattern/MVVM_pattern/ViewModel/MvvmViewModel.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+protocol MvvmViewModelProtocol: class {
+    var updateView: ((MvvmView)->())? { get set }
+    
+    func showFirstImage()
+    func showSecondIamge()
+}
+
+class MvvmViewModel: MvvmViewModelProtocol {
+    var updateView: ((MvvmView) -> ())?
+    
+    func showFirstImage() {
+        
+    }
+    
+    func showSecondIamge() {
+        
+    }
+}
